2cb32c531e4678eb6bdb194e22c8ab504205166a
Kirankumar Gadagi <kirankumar.gadagi@iping.in>
2020-06-28 20:20:17 +0530

    chips design changed

--- resources/views/buy_chips.blade.php
+++ resources/views/buy_chips.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    div {
+        color: #000000;
+    }
+</style>
 
 <div class="container">
     <div class="row maincontent">
@@ -11,9 +16,24 @@
                     <span class="card-title center" style="padding-top: 25px; margin-bottom: 0;">Add Chips via
                         Paytm</span>
                     <form style="padding-bottom: 0;">
+                        <div class="col s12 m12 l12">
+                            <label for="upi_id">
+                                UPI
+                            </label>
+
+                            <input id="upi_id" type="text" class="validate" value="test@upi" readonly>
+
+                            <p>
+                            </p>
+                            <li>Minimum Deposit: <strong>50 Chips</strong></li>
+                            <li class="text-warning">UPI ID changes regularly, Always check before sending money.</li>
+                            <li class="text-success">Money will be credited on account immediately</li>
+                            <p></p>
+                        </div>
                         <div class="input-field" style="margin-top: 0;">
                             <input disabled id="disabled" type="number" class="validate">
-                            <div>For automate transfer of money into your account. Send money by <b>PAYTM</b> to the
+                            <div class="text-info">For automate transfer of money into your account. Send money by
+                                <b>PAYTM</b> to the
                                 number: <span id="adminNo"></span>. Enter the Transaction ID. Minimum Deposit: 50 Chips
                                 Our payment number change regularly, Dont Save this no! <b>Use PAYTM only, No Google
                                     Pay, Phone Pe, Etc.</b>
