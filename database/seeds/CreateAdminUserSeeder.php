1fb57459f3401a3ce81e3c9ee731a046233fde04
Kirankumar Gadagi <kirankumar.gadagi@iping.in>
2020-06-28 18:03:28 +0530

    branch created

--- database/seeds/CreateAdminUserSeeder.php
+++ database/seeds/CreateAdminUserSeeder.php
@@ -1,10 +1,10 @@
 <?php
-  
+
 use Illuminate\Database\Seeder;
 use App\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-  
+
 class CreateAdminUserSeeder extends Seeder
 {
     /**
@@ -15,17 +15,17 @@ class CreateAdminUserSeeder extends Seeder
     public function run()
     {
         $user = User::create([
-        	'name' => 'Hardik Savani', 
+        	'name' => 'Admin ', 
         	'email' => 'admin@gmail.com',
         	'password' => bcrypt('123456')
         ]);
-  
+
         $role = Role::create(['name' => 'Admin']);
-   
+
         $permissions = Permission::pluck('id','id')->all();
-  
+
         $role->syncPermissions($permissions);
-   
+
         $user->assignRole([$role->id]);
     }
 }
--- database/seeds/PermissionTableSeeder.php
+++ database/seeds/PermissionTableSeeder.php
@@ -15,16 +15,12 @@ class PermissionTableSeeder extends Seeder
     public function run()
     {
        $permissions = [
-           'role-list',
-           'role-create',
-           'role-edit',
-           'role-delete',
-           'product-list',
-           'product-create',
-           'product-edit',
-           'product-delete'
+           'user-list',
+           'user-create',
+           'user-edit',
+           'user-delete',
         ];
-   
+
         foreach ($permissions as $permission) {
              Permission::create(['name' => $permission]);
         }
